566d9fc453380e83ca69a144da289d8704de8285
Antoine Gagnon <antoine.gagnon760@gmail.com>
2019-08-14 21:30:47 -0400

    Add db

---
+++ src/db/sqlite.js
@@ -0,0 +1,14 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./data/images.db', (err) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  
+  db.run("CREATE TABLE IF NOT EXISTS images (id INTEGER PRIMARY KEY, url varchar(64), caption TEXT);")
+  
+  console.log('Connected to the image database.');
+});
+
+module.exports = db;
