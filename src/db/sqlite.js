bf423296e42264e186f95433a93181771e441dd1
Antoine Gagnon <antoine.gagnon760@gmail.com>
2019-08-15 17:29:44 -0400

    Store image DB in root directory

--- src/db/sqlite.js
+++ src/db/sqlite.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./data/images.db', (err) => {
+const db = new sqlite3.Database('./images.db', (err) => {
   if (err) {
     console.error(err.message);
     return;
